80f590cd63c8aba158c1b4a1a70ff05a2ae313cc
edin <edo.truman@gmail.com>
2021-03-03 21:36:07 +0100

    Footer

--- client/src/components/BasicTable.js
+++ client/src/components/BasicTable.js
@@ -12,7 +12,14 @@ export const BasicTable = () => {
     data
   })
 
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = tableInstance
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    footerGroups,
+    rows,
+    prepareRow
+  } = tableInstance
 
   return (
     <table {...getTableProps()}>
@@ -37,6 +44,15 @@ export const BasicTable = () => {
           )
         })}
       </tbody>
+      <tfoot>
+        {footerGroups.map(footerGroup => (
+          <tr {...footerGroup.getFooterGroupProps()}>
+            {footerGroup.headers.map(column => (
+              <td {...column.getFooterProps}>{column.render("Footer")}</td>
+            ))}
+          </tr>
+        ))}
+      </tfoot>
     </table>
   )
 }
--- client/src/components/columns.js
+++ client/src/components/columns.js
@@ -1,23 +1,28 @@
 export const COLUMNS = [
   {
     Header: "Datum",
+    Footer: "Datum",
     accessor: "datum1"
   },
-  { Header: "Id", accessor: "id" },
+  { Header: "Id", Footer: "Id", accessor: "id" },
   {
     Header: "Name",
+    Footer: "Name",
     accessor: "name"
   },
   {
     Header: "Team",
+    Footer: "Team",
     accessor: "team"
   },
   {
     Header: "Soll",
+    Footer: "Soll",
     accessor: "soll_1"
   },
   {
     Header: "Ist",
+    Footer: "Ist",
     accessor: "ist_1"
   }
 ]
