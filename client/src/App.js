279460e575cd477fca92275fe459e9f9d01a6562
edin <edo.truman@gmail.com>
2021-03-10 18:51:15 +0100

    row selection

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,20 @@ import "./App.css"
 // import { FilteringTable } from "./components/FilteringTable"
 // import { SortingTable } from "./components/SortingTable"
 // import { BasicTable } from "./components/BasicTable"
-import { PaginationTable } from "./components/PaginationTable"
+// import { PaginationTable } from "./components/PaginationTable"
+import { RowSelection } from "./components/RowSelection"
 
+// function App() {
+//   return (
+//     <div className="App">
+//       <RowSelection />
+//     </div>
+//   )
+// }
 function App() {
   return (
-    <div className="App">
-      <PaginationTable />
+    <div>
+      <RowSelection />
     </div>
   )
 }
---
+++ client/src/components/RowSelection.js
@@ -0,0 +1,79 @@
+import React, { useMemo } from "react"
+import { useTable, useRowSelect } from "react-table"
+import Data from "./db.json"
+import { COLUMNS } from "./columns"
+import "./table.css"
+import { CheckBox } from "./CheckBox"
+
+export const RowSelection = () => {
+  const columns = useMemo(() => COLUMNS, [])
+  const data = useMemo(() => Data, [])
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    selectedFlatRows
+  } = useTable(
+    {
+      columns,
+      data
+    },
+    useRowSelect,
+    hooks => {
+      hooks.visibleColumns.push(columns => [
+        {
+          id: "selection",
+          Header: ({ getToggleAllRowsSelectedProps }) => (
+            <CheckBox {...getToggleAllRowsSelectedProps()} />
+          ),
+          Cell: ({ row }) => <CheckBox {...row.getToggleRowSelectedProps()} />
+        },
+        ...columns
+      ])
+    }
+  )
+
+  const firstPageRows = rows.slice(0, 10)
+
+  return (
+    <>
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {firstPageRows.map(row => {
+            prepareRow(row)
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell => {
+                  return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                })}
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+      <pre>
+        <code>
+          {JSON.stringify(
+            {
+              selectedFlatRows: selectedFlatRows.map(row => row.original)
+            },
+            null,
+            2
+          )}
+        </code>
+      </pre>
+    </>
+  )
+}
